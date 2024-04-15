3c8a855919eff6fa0a44681213c8c2b133389009
Andrew Wang <wardengnaw@gmail.com>
2024-04-15 11:21:57 -0700

    Update to April 2024 dotnet new aspire-starter

--- AspireDemoProject.AppHost/Program.cs
+++ AspireDemoProject.AppHost/Program.cs
@@ -3,6 +3,7 @@ var builder = DistributedApplication.CreateBuilder(args);
 var apiService = builder.AddProject<Projects.AspireDemoProject_ApiService>("apiservice");
 
 builder.AddProject<Projects.AspireDemoProject_Web>("webfrontend")
+    .WithExternalHttpEndpoints()
     .WithReference(apiService);
 
 builder.Build().Run();
